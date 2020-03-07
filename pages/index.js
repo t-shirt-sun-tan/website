e2228a3e0077c672b1de160b000d3bc483102c89
Koji Kobayashi <cozynooks@gmail.com>
2020-03-08 00:17:49 +0900

    Layout center.

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,29 @@ if (typeof window !== `undefined`) {
 }
 
 const Home = () => (
-  <div className="container">
+  <Container>
     <main>
       <Title>
         T-Shirt Sun Tan
       </Title>
     </main>
-  </div>
+  </Container>
 )
+
+const Container = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Title = styled.h1`
   font-family: azo-sans-web, sans-serif;
   font-weight: 400;
   font-style: normal;
-  font-size: 1.5em;
+  font-size: 3em;
   text-align: center;
   color: #1a1a1a;
 ` 
